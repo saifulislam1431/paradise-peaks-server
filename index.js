0b05205bc792a1a024e166fece0577ca81611262
Saiful Islam <sakibbhaiya143@gmail.com>
2023-04-24 17:27:28 +0600

    All room json added

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require("express");
 const app = express();
 const port = 7000;
 const cors = require("cors");
-
+const allRooms = require("./hotelData.json");
 app.use(cors());
 
 app.get("/",(req,res)=>{
     res.send("The Paradise Peaks server");
 })
-
+app.get("/allRooms",(req , res)=>{
+    res.send(allRooms);
+})
 app.listen(port,()=>{
     console.log(`This server listening at port: ${port}`);
 })
